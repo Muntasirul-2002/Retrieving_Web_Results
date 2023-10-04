28b87f4d4d92cb2dfb260c13ea2c8201ce894933
Muntasirul islam <108189526+Muntasirul-2002@users.noreply.github.com>
2023-10-04 13:42:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function search() {
+    const keyword = document.getElementById("searchInput").value;
+    // Send an API request to your backend with the keyword
+    // Update the "resultsContainer" with the search results
+}
