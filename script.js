f20d269287cb1dafcd1067608e0426338ab84e67
Muntasirul islam <108189526+Muntasirul-2002@users.noreply.github.com>
2023-10-05 09:47:00 +0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function search() {
-    const keyword = document.getElementById("searchInput").value;
-    // Send an API request to your backend with the keyword
-    // Update the "resultsContainer" with the search results
-}
